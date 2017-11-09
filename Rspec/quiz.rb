709629f96002dc40b7984413d85a7b3955fbd5d5
Ji Hyuk Kim <gskjhj@naver.com>
2017-11-09 05:07:03 +0000

    edit quiz.rb

--- Rspec/quiz.rb
+++ Rspec/quiz.rb
@@ -7,6 +7,7 @@ end
 
 # sum of maximum element in array up to second
 def max_2_sum arr
+  # arr.max(2).sum()
   return arr.sort.reverse[0,2].sum() # 내림차순 sort하고 2개 합
 end
 
@@ -20,6 +21,7 @@ def sum_to_n? arr, n
   # end
   # return result
   arr.combination(2).to_a.map {|x| x.sum()}.include? n
+  # arr.combination(2).any? {|a,b| a+b==n}
 end
 
 # Part 2
@@ -31,5 +33,8 @@ end
 
 # return true if string start with consonant
 def starts_with_consonant? s
-  if /^[^aeiou\W]/i.match(s) == nil then false else true end
+  # if /^[^aeiou\W]/i.match(s) == nil then false else true end
+  /^[^aeiou\W]/i.match(s)==nil ? false : true
+  # !%w(a e i o u).include? s[0].downcase unless s.empty? or /^\W/.match(s)
+  
 end
